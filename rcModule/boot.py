616234366117f54094faa4e26c3a1fe0bf5ead6a
Preaw1005 <6238195521@student.chula.ac.th>
2023-04-18 14:59:26 +0700

    new branch

--- rcModule/boot.py
+++ rcModule/boot.py
@@ -1,8 +1,10 @@
 import utime
 import network
+import socket
+from machine import Pin
 
-ssid = 'poonL.'
-password = '20010728'
+ssid = 'Preawwyp'
+password = 'Preaw10944'
 
 # set Wi-Fi to station interface
 wlan = network.WLAN(network.STA_IF)
@@ -37,8 +39,57 @@ else:
     # connection successful
     print('wlan connected')
     status = wlan.ifconfig()
+    # pico_ip = status[0]
     print('IP address = ' + status[0])
     print('subnet mask = ' + status[1])
     print('gateway  = ' + status[2])
     print('DNS server = ' + status[3])
 
+# # Open socket
+# # addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+# addr = (pico_ip, 80)
+# s = socket.socket()
+# s.bind(addr)
+# s.listen(1)
+# print('listening on', addr)
+
+# led = Pin(28, Pin.OUT)
+# led.off()
+# led_state = False
+
+# # main loop
+# while True:
+#     client, client_addr = s.accept()
+#     raw_request = client.recv(1024)
+#     # translate byte string to normal string variable
+#     raw_request = raw_request.decode("utf-8")
+#     print(raw_request)
+
+#     # break request into words (split at spaces)
+#     request_parts = raw_request.split()
+#     http_method = request_parts[0]
+#     request_url = request_parts[1]
+
+#     if request_url.find("/ledon") != -1:
+#         # turn LED on
+#         led_state = True
+#         led.on()
+#     elif request_url.find("/ledoff") != -1:
+#         # turn LED off
+#         led_state = False
+#         led.off()
+#     else:
+#         # do nothing
+#         pass
+
+#     led_state_text = "OFF"
+#     if led_state:
+#         led_state_text = "ON"
+
+#     file = open("webpage.html")
+#     html = file.read()
+#     file.close()
+
+#     html = html.replace('**ledState**', led_state_text)
+#     client.send(html)
+#     client.close()
